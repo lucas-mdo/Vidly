9c8c8c569b3a8acb8f05fac7498bad4c5598fdf3
lucasmdo <luketeoli@hotmail.com>
2017-05-30 15:37:34 -0300

    Added Birthdate to Customer, update the database and Details view. Exercise 2.2 done.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
@@ -9,6 +10,7 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
+        public DateTime? Birthdate { get; set; }
         public MembershipType MembershipType { get; set; }
         //Foreign key
         public byte MembershipTypeId { get; set; }  
