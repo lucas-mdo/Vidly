47fe0a6123d4ffbd776dc38e25b6c81afd1d9f5b
lucasmdo <luketeoli@hotmail.com>
2017-05-30 14:31:40 -0300

    Using CodeFirst EF approach to build and seed the database.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -4,5 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        //Foreign key
+        public byte MembershipTypeId { get; set; }  
     }
 }
