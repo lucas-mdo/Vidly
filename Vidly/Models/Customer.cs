5fc4ce5471b7d819f91e37f2bf07b58cd44ec295
lucasmdo <luketeoli@hotmail.com>
2017-05-31 11:48:01 -0300

    Added a New page to add new customers. Change display label for Birthdate in the Customer model.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,6 +10,7 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
         public MembershipType MembershipType { get; set; }
         //Foreign key
