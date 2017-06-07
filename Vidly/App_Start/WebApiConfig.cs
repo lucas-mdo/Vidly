cf82fa73403167d308bcff2610ecc76d0ff5d508
lucasmdo <luketeoli@hotmail.com>
2017-06-07 12:54:16 -0300

    Changed web api settings to use camel notation for JSON responses.

--- Vidly/App_Start/WebApiConfig.cs
+++ Vidly/App_Start/WebApiConfig.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Vidly
 {
@@ -9,6 +8,11 @@ namespace Vidly
     {
         public static void Register(HttpConfiguration config)
         {
+            //Change settings to Camel Notation (first letter is lower case)
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
