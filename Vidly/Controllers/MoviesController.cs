fe73ad7686d797e6ca05d45287bc54b1d65d7f01
lucasmdo <luketeoli@hotmail.com>
2017-06-07 10:57:12 -0300

    Refactored code to have nullable required fields in Movie form and avoid weird default values.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -65,9 +65,8 @@ namespace Vidly.Controllers
             if (movie == null)
                 return HttpNotFound();
 
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
                 Genres = _context.Genres.ToList()
             };
 
@@ -86,9 +85,8 @@ namespace Vidly.Controllers
             if (!ModelState.IsValid)
             {
                 //Keep filled data in the page
-                var viewModel = new MovieFormViewModel
+                var viewModel = new MovieFormViewModel(movie)
                 {
-                    Movie = movie,
                     Genres = _context.Genres.ToList()
                 };
 
--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Vidly.Models;
 
 namespace Vidly.ViewModels
@@ -6,17 +8,46 @@ namespace Vidly.ViewModels
     public class MovieFormViewModel
     {
         public IEnumerable<Genre> Genres { get; set; }
-        public Movie Movie { get; set; }
+        public int? Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        //Foreign Key
+        [Display(Name = "Genre")]
+        [Required]
+        public byte? GenreId { get; set; }
+
+        [Display(Name = "Release Date")]
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Display(Name = "Quantity in Stock")]
+        [Range(1, 20)]
+        [Required]
+        public byte? QuantityInStock { get; set; }
 
         public string Title
         {
             get
             {
-                if (Movie != null && Movie.Id != 0)
-                    return "Edit Movie";
-
-                return "New Movie";
+                return Id != 0 ? "Edit Movie" : "New Movie";
             }
+        }
+
+        public MovieFormViewModel()
+        {
+            Id = 0;
+        }
+
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            ReleaseDate = movie.ReleaseDate;
+            QuantityInStock = movie.QuantityInStock;
+            GenreId = movie.GenreId;
         }
     }
 }
